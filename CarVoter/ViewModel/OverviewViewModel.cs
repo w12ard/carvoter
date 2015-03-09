b606b7ea53f64e2205928f397ec26a5b70e65acb
w12ard <info@ahkim.com>
2015-03-10 06:32:30 +0800

    Initial commit

---
+++ CarVoter/ViewModel/OverviewViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CarVoter.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarVoter.ViewModel
+{
+    class OverviewViewModel
+    {
+        public IList<Car> Cars { get; set; }
+    }
+}
